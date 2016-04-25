69dcfe156064a4ca5731b7840ae8fd9c8819a9d5
Bart van der Zwet <bart.van.der.zwet@cgi.com>
2016-04-25 18:44:04 +0200

    Sends event notifications based on EvnRpn reports by Kaifa device.

--- osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/application/services/DeviceManagementService.java
+++ osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/application/services/DeviceManagementService.java
@@ -7,6 +7,8 @@
  */
 package com.alliander.osgp.adapter.protocol.iec61850.application.services;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.alliander.osgp.adapter.protocol.iec61850.exceptions.ProtocolAdapterException;
 import com.alliander.osgp.adapter.protocol.iec61850.infra.messaging.OsgpRequestMessageSender;
+import com.alliander.osgp.core.db.api.iec61850.entities.DeviceOutputSetting;
 import com.alliander.osgp.core.db.api.iec61850.entities.Ssld;
 import com.alliander.osgp.core.db.api.iec61850.repositories.SsldDataRepository;
 import com.alliander.osgp.dto.valueobjects.DeviceFunction;
@@ -23,7 +26,7 @@ import com.alliander.osgp.dto.valueobjects.EventType;
 import com.alliander.osgp.shared.infra.jms.RequestMessage;
 
 @Service(value = "iec61850DeviceManagementService")
-@Transactional(value = "transactionManager")
+@Transactional(value = "iec61850OsgpCoreDbApiTransactionManager")
 public class DeviceManagementService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DeviceManagementService.class);
@@ -42,7 +45,10 @@ public class DeviceManagementService {
     }
 
     /**
-     * Send a new event notification to OSGP Core with the given arguments.
+     * Send a new event notification to OSGP Core with the given arguments.<br>
+     * Note that a non-zero internal index will be mapped to an external index
+     * using the DeviceOutputSettings for the event notification that is sent to
+     * the platform. If no mapping is available the external index will be 0.
      *
      * @param deviceIdentification
      *            The identification of the device
@@ -51,12 +57,12 @@ public class DeviceManagementService {
      * @param description
      *            The description which came along with the event from the
      *            device.
-     * @param index
-     *            The index of the device.
+     * @param internalIndex
+     *            The internal index (switch number) of the device.
      * @throws ProtocolAdapterException
      */
     public void addEventNotification(final String deviceIdentification, final String eventType,
-            final String description, final Integer index) throws ProtocolAdapterException {
+            final String description, final Integer internalIndex) throws ProtocolAdapterException {
 
         final Ssld ssldDevice = this.ssldDataRepository.findByDeviceIdentification(deviceIdentification);
         if (ssldDevice == null) {
@@ -64,11 +70,34 @@ public class DeviceManagementService {
                     + deviceIdentification);
         }
 
-        LOGGER.info("addEventNotification called for device: {} with eventType: {}, description: {} and index: {}",
-                deviceIdentification, eventType, description, index);
+        LOGGER.info(
+                "addEventNotification called for device: {} with eventType: {}, description: {} and internal index: {}",
+                deviceIdentification, eventType, description, internalIndex);
+
+        int externalIndex = 0;
+        if (internalIndex != null && internalIndex != 0) {
+            final List<DeviceOutputSetting> outputSettings = ssldDevice.getOutputSettings();
+            for (final DeviceOutputSetting outputSetting : outputSettings) {
+                if (internalIndex == outputSetting.getInternalId()) {
+                    externalIndex = outputSetting.getExternalId();
+                    break;
+                }
+            }
+            if (externalIndex == 0) {
+                LOGGER.warn(
+                        "addEventNotification could not map internal index: {} to an external index for device: {} with eventType: {}",
+                        internalIndex, deviceIdentification, eventType);
+            }
+        }
+
+        if (externalIndex > 0) {
+            LOGGER.info(
+                    "addEventNotification mapped internal index: {} to external index: {}, for device: {} with eventType: {}",
+                    internalIndex, externalIndex, deviceIdentification, eventType);
+        }
 
         final EventNotification eventNotification = new EventNotification(deviceIdentification,
-                EventType.valueOf(eventType), description, index);
+                EventType.valueOf(eventType), description, externalIndex);
         final RequestMessage requestMessage = new RequestMessage("no-correlationUid", "no-organisation",
                 deviceIdentification, eventNotification);
 
--- osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/infra/networking/Iec61850ClientEventListener.java
+++ osgp-protocol-adapter-iec61850/src/main/java/com/alliander/osgp/adapter/protocol/iec61850/infra/networking/Iec61850ClientEventListener.java
@@ -22,37 +22,123 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.alliander.osgp.adapter.protocol.iec61850.application.services.DeviceManagementService;
+import com.alliander.osgp.adapter.protocol.iec61850.exceptions.ProtocolAdapterException;
+import com.alliander.osgp.dto.valueobjects.EventType;
 
 public class Iec61850ClientEventListener implements ClientEventListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Iec61850ClientEventListener.class);
 
+    /**
+     * The EntryTime from IEC61850 has timestamp values relative to 01-01-1984.
+     * TimeStamp values and Java date time values have milliseconds since
+     * 01-01-1970. The milliseconds between these representations are in the
+     * following offset.
+     */
+    private static final long IEC61850_ENTRY_TIME_OFFSET = 441763200000L;
+
     private static final Map<Short, String> EVN_TYPE_DESCRIPTION_PER_CODE = new TreeMap<>();
+    private static final Map<Short, EventType> OSGP_EVENT_TYPE_PER_CODE = new TreeMap<>();
     private static final Map<Short, String> TRG_TYPE_DESCRIPTION_PER_CODE = new TreeMap<>();
 
+    /*-
+     * OSGP Event types not mapped from IEC61850 evnType:
+     *
+     * HARDWARE_FAILURE_RELAY(1)
+     * LIGHT_FAILURE_DALI_COMMUNICATION(2)
+     * LIGHT_FAILURE_BALLAST(3)
+     * MONITOR_EVENTS_LONG_BUFFER_FULL(6)
+     * LIGHT_FAILURE_TARIFF_SWITCH_ATTEMPT(10)
+     * MONITOR_FAILURE_P1_COMMUNICATION(13)
+     * COMM_EVENTS_ALTERNATIVE_CHANNEL(14)
+     * COMM_EVENTS_RECOVERED_CHANNEL(15)
+     * SECURITY_EVENTS_OUT_OF_SEQUENCE(16)
+     * MONITOR_SHORT_DETECTED(17)
+     * MONITOR_SHORT_RESOLVED(18)
+     * MONITOR_DOOR_OPENED(19)
+     * MONITOR_DOOR_CLOSED(20)
+     * ALARM_NOTIFICATION(21)
+     * SMS_NOTIFICATION(22)
+     * MONITOR_EVENTS_TEST_RELAY_ON(23)
+     * MONITOR_EVENTS_TEST_RELAY_OFF(24)
+     * MONITOR_EVENTS_LOCAL_MODE(26)
+     * MONITOR_EVENTS_REMOTE_MODE(27)
+     * FIRMWARE_EVENTS_CONFIGURATION_CHANGED(28)
+     *
+     *
+     * OSGP Event types mapped from multiple IEC61850 evnType:
+     *
+     * FIRMWARE_EVENTS_ACTIVATING(7)
+     *   from  7 - FUNCTION_FIRMWARE_EVENTS_ACTIVATING
+     *   and  11 - SECURITY_FIRMWARE_EVENTS_ACTIVATING
+     *
+     * FIRMWARE_EVENTS_DOWNLOAD_NOTFOUND(8)
+     *   from  8 - FUNCTION_FIRMWARE_EVENTS_DOWNLOAD_NOTFOUND
+     *   and  12 - SECURITY_FIRMWARE_EVENTS_DOWNLOAD_NOTFOUND
+     *
+     * FIRMWARE_EVENTS_DOWNLOAD_FAILED(9)
+     *   from  9 - FUNCTION_FIRMWARE_EVENTS_DOWNLOAD_FAILED
+     *   and  13 - SECURITY_FIRMWARE_EVENTS_DOWNLOAD_FAILED
+     *
+     *
+     * IEC61850 evnType without OSGP Event type
+     *
+     * 10 - FUNCTION_FIRMWARE_EVENTS_DOWNLOAD_SUCCESS
+     * 14 - SECURITY_FIRMWARE_EVENTS_DOWNLOAD_SUCCESS
+     * 15 - CA_FILE_EVENTS_ACTIVATING
+     * 16 - CA_FILE_FIRMWARE_EVENTS_DOWNLOAD_NOTFOUND
+     * 17 - CA_FILE_EVENTS_DOWNLOAD_FAILED
+     * 18 - CA_FILE_EVENTS_DOWNLOAD_SUCCESS
+     * 19 - NTP_SERVER_NOT_REACH
+     * 20 - NTP_SYNC_ALARM_OFFSET
+     * 21 - NTP_SYNC_MAX_OFFSET
+     * 22 - AUTHENTICATION_FAIL
+     */
     static {
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 1, "DIAG_EVENTS_GENERAL");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 1, EventType.DIAG_EVENTS_GENERAL);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 2, "LIGHT_EVENTS_LIGHT_ON");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 2, EventType.LIGHT_EVENTS_LIGHT_ON);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 3, "LIGHT_EVENTS_LIGHT_OFF");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 3, EventType.LIGHT_EVENTS_LIGHT_OFF);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 4, "TARIFF_EVENTS_TARIFF_ON");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 4, EventType.TARIFF_EVENTS_TARIFF_ON);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 5, "TARIFF_EVENTS_TARIFF_OFF");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 5, EventType.TARIFF_EVENTS_TARIFF_OFF);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 6, "MONITOR_EVENTS_LOSS_OF_POWER");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 6, EventType.MONITOR_EVENTS_LOSS_OF_POWER);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 7, "FUNCTION_FIRMWARE_EVENTS_ACTIVATING");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 7, EventType.FIRMWARE_EVENTS_ACTIVATING);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 8, "FUNCTION_FIRMWARE_EVENTS_DOWNLOAD_NOTFOUND");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 8, EventType.FIRMWARE_EVENTS_DOWNLOAD_NOTFOUND);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 9, "FUNCTION_FIRMWARE_EVENTS_DOWNLOAD_FAILED");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 9, EventType.FIRMWARE_EVENTS_DOWNLOAD_FAILED);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 10, "FUNCTION_FIRMWARE_EVENTS_DOWNLOAD_SUCCESS");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 10, null);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 11, "SECURITY_FIRMWARE_EVENTS_ACTIVATING");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 11, EventType.FIRMWARE_EVENTS_ACTIVATING);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 12, "SECURITY_FIRMWARE_EVENTS_DOWNLOAD_NOTFOUND");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 12, EventType.FIRMWARE_EVENTS_DOWNLOAD_NOTFOUND);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 13, "SECURITY_FIRMWARE_EVENTS_DOWNLOAD_FAILED");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 13, EventType.FIRMWARE_EVENTS_DOWNLOAD_FAILED);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 14, "SECURITY_FIRMWARE_EVENTS_DOWNLOAD_SUCCESS");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 14, null);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 15, "CA_FILE_EVENTS_ACTIVATING");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 15, null);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 16, "CA_FILE_FIRMWARE_EVENTS_DOWNLOAD_NOTFOUND");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 16, null);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 17, "CA_FILE_EVENTS_DOWNLOAD_FAILED");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 17, null);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 18, "CA_FILE_EVENTS_DOWNLOAD_SUCCESS");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 18, null);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 19, "NTP_SERVER_NOT_REACH");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 19, null);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 20, "NTP_SYNC_ALARM_OFFSET");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 20, null);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 21, "NTP_SYNC_MAX_OFFSET");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 21, null);
         EVN_TYPE_DESCRIPTION_PER_CODE.put((short) 22, "AUTHENTICATION_FAIL");
+        OSGP_EVENT_TYPE_PER_CODE.put((short) 22, null);
 
         TRG_TYPE_DESCRIPTION_PER_CODE.put((short) 1, "light trigger (sensor trigger)");
         TRG_TYPE_DESCRIPTION_PER_CODE.put((short) 2, "ad-hoc trigger");
@@ -75,8 +161,110 @@ public class Iec61850ClientEventListener implements ClientEventListener {
 
     @Override
     public void newReport(final Report report) {
-        LOGGER.info("newReport for device: {}, reportId: {}", this.deviceIdentification, report.getRptId());
+        final String reportDescription = String.format("device: %s, reportId: %s, timeOfEntry: %s, sqNum: %s%s%s",
+                this.deviceIdentification, report.getRptId(), report.getTimeOfEntry() == null ? "-" : new DateTime(
+                        report.getTimeOfEntry().getTimestampValue() + IEC61850_ENTRY_TIME_OFFSET), report.getSqNum(),
+                        report.getSubSqNum() == null ? "" : " subSqNum: " + report.getSubSqNum(),
+                                report.isMoreSegmentsFollow() ? " (more segments follow for this sqNum)" : "");
+        LOGGER.info("newReport for {}", reportDescription);
+        if (report.isBufOvfl()) {
+            LOGGER.warn("Buffer Overflow reported for {} - entries within the buffer may have been lost.",
+                    reportDescription);
+        }
         this.logReportDetails(report);
+
+        final DataSet dataSet = report.getDataSet();
+        if (dataSet == null) {
+            LOGGER.warn("No DataSet available for {}", reportDescription);
+            return;
+        }
+        final List<FcModelNode> members = dataSet.getMembers();
+        if (members == null || members.isEmpty()) {
+            LOGGER.warn("No members in DataSet available for {}", reportDescription);
+            return;
+        } else {
+            LOGGER.debug("Handling {} DataSet members for {}", members.size(), reportDescription);
+        }
+        for (final FcModelNode member : members) {
+            if (member == null) {
+                LOGGER.warn("Member == null in DataSet for {}", reportDescription);
+                continue;
+            }
+            LOGGER.info("Handle member {} for {}", member.getReference(), reportDescription);
+            try {
+                this.addEventNotificationForReportedData(member, reportDescription);
+            } catch (final Exception e) {
+                LOGGER.error("Error adding event notification for member {} from {}", member.getReference(),
+                        reportDescription, e);
+            }
+        }
+    }
+
+    private void addEventNotificationForReportedData(final FcModelNode evnRpn, final String reportDescription)
+            throws ProtocolAdapterException {
+
+        final EventType eventType = this.determineEventType(evnRpn, reportDescription);
+        final Integer index = this.determineRelayIndex(evnRpn, reportDescription);
+        final String description = this.determineDescription(evnRpn, reportDescription);
+
+        this.deviceManagementService.addEventNotification(this.deviceIdentification, eventType.name(), description,
+                index);
+    }
+
+    private EventType determineEventType(final FcModelNode evnRpn, final String reportDescription) {
+
+        final String evnTypeName = "evnType";
+        final BdaInt8U evnTypeNode = (BdaInt8U) evnRpn.getChild(evnTypeName);
+        if (evnTypeNode == null) {
+            throw this.childNodeNotAvailableException(evnRpn, evnTypeName, reportDescription);
+        }
+
+        final Short evnTypeCode = evnTypeNode.getValue();
+        final EventType eventType = OSGP_EVENT_TYPE_PER_CODE.get(evnTypeCode);
+
+        if (eventType == null) {
+            final String exceptionMessage;
+            if (OSGP_EVENT_TYPE_PER_CODE.containsKey(evnTypeCode)) {
+                exceptionMessage = "No event mapping available for evnType: " + evnTypeCode + "("
+                        + EVN_TYPE_DESCRIPTION_PER_CODE.get(evnTypeCode) + ") from " + reportDescription;
+            } else {
+                exceptionMessage = "Unknown evnType: " + evnTypeCode + " from " + reportDescription;
+            }
+            throw new IllegalArgumentException(exceptionMessage);
+        }
+
+        return eventType;
+    }
+
+    private Integer determineRelayIndex(final FcModelNode evnRpn, final String reportDescription) {
+
+        final String swNumName = "swNum";
+        final BdaInt8U swNumNode = (BdaInt8U) evnRpn.getChild(swNumName);
+        if (swNumNode == null) {
+            throw this.childNodeNotAvailableException(evnRpn, swNumName, reportDescription);
+        }
+
+        final Short swNum = swNumNode.getValue();
+
+        return swNum.intValue();
+    }
+
+    private String determineDescription(final FcModelNode evnRpn, final String reportDescription) {
+
+        final String remarkName = "remark";
+        final BdaVisibleString remarkNode = (BdaVisibleString) evnRpn.getChild(remarkName);
+        if (remarkNode == null) {
+            throw this.childNodeNotAvailableException(evnRpn, remarkName, reportDescription);
+        }
+
+        return remarkNode.getStringValue();
+    }
+
+    private IllegalArgumentException childNodeNotAvailableException(final FcModelNode evnRpn,
+            final String childNodeName,
+            final String reportDescription) {
+        return new IllegalArgumentException("No '" + childNodeName + "' child in DataSet member "
+                + evnRpn.getReference() + " from " + reportDescription);
     }
 
     private void logReportDetails(final Report report) {
@@ -94,7 +282,8 @@ public class Iec61850ClientEventListener implements ClientEventListener {
         sb.append("\t          SubSqNum:\t").append(report.getSubSqNum()).append(System.lineSeparator());
         sb.append("\t       TimeOfEntry:\t").append(report.getTimeOfEntry()).append(System.lineSeparator());
         if (report.getTimeOfEntry() != null) {
-            sb.append("\t                   \t(").append(new DateTime(report.getTimeOfEntry().getTimestampValue()))
+            sb.append("\t                   \t(")
+            .append(new DateTime(report.getTimeOfEntry().getTimestampValue() + IEC61850_ENTRY_TIME_OFFSET))
             .append(')').append(System.lineSeparator());
         }
         final List<BdaReasonForInclusion> reasonCodes = report.getReasonCodes();
