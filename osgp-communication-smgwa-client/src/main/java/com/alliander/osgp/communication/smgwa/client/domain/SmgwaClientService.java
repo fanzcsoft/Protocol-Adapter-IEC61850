fc68faba07903025eaa349289328a30373d3a325
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2016-06-01 12:37:00 +0200

    CUC-20 ~ Corrects parameters

--- osgp-communication-smgwa-client/src/main/java/com/alliander/osgp/communication/smgwa/client/domain/SmgwaClientService.java
+++ osgp-communication-smgwa-client/src/main/java/com/alliander/osgp/communication/smgwa/client/domain/SmgwaClientService.java
@@ -1,9 +1,9 @@
 package com.alliander.osgp.communication.smgwa.client.domain;
 
 public interface SmgwaClientService {
-    void configurePlatformCommunicationProfile(String PlatformIdentification, PlatformCommunicationProfile profile);
+    void configurePlatformCommunicationProfile(String platformIdentification, PlatformCommunicationProfile profile);
 
-    void configureDeviceCommunicationProfile(String DeviceIdentification, DeviceCommunicationProfile profile);
+    void configureDeviceCommunicationProfile(String deviceIdentification, DeviceCommunicationProfile profile);
 
-    void configureProxyServer(String SmartmeterGatewayIdentification, ProxyServer proxy);
+    void configureProxyServer(String smartmeterGatewayIdentification, ProxyServer proxy);
 }
