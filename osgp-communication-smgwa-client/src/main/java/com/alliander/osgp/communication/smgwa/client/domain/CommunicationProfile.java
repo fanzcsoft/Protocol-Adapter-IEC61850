de8516b6128d3142d5d4536904483b805ad99db0
Sander van der Heijden <sander.van.der.heijden@cgi.com>
2016-06-02 10:17:19 +0200

    CUC-18 ~ Updates getConfigurationTime

--- osgp-communication-smgwa-client/src/main/java/com/alliander/osgp/communication/smgwa/client/domain/CommunicationProfile.java
+++ osgp-communication-smgwa-client/src/main/java/com/alliander/osgp/communication/smgwa/client/domain/CommunicationProfile.java
@@ -58,7 +58,7 @@ public abstract class CommunicationProfile extends AbstractEntity {
         return this.configured;
     }
 
-    public Date configurationTime() {
+    public Date getConfigurationTime() {
         return this.configurationTime;
     }
 
